ba8970435a8235125ed60475b2aa711e215f66dc
Teasani <72011173+Teasani@users.noreply.github.com>
2020-09-28 16:02:10 +0300

    tehtävät

--- 1-4/index.php
+++ 1-4/index.php
@@ -2,3 +2,14 @@
 $a = 8;
 $b = 4;
 
+$merkit1 = $a + $b;
+$merkit2 = $a - $b;
+$merkit3 = $a * $b;
+$merkit4 = $a / $b;
+$merkit5 = $a % $b;
+
+print "$a + $b = $merkit1" . "<br>";
+print "$a - $b = $merkit2" . "<br>";
+print "$a * $b = $merkit3" . "<br>";
+print "$a / $b = $merkit4" . "<br>";
+print "$a % $b = $merkit5";
--- 1-5/index.php
+++ 1-5/index.php
@@ -1,1 +1,5 @@
 <?php
+$array["1"] = 'A', 'B', 'C','D', 'E', 'F', 'G', 'H', 'I', 'J', 'K';
+$array["2"] = 'K', 'J', 'I','H', 'G', 'F', 'E', 'D', 'C', 'B', 'A';
+
+print $array . "<br>";
