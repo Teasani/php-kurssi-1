c8a6df9eb05bfd0cbafdc16d83f24b5b5c65bd6e
Teasani <72011173+Teasani@users.noreply.github.com>
2020-09-29 08:37:41 +0300

    1-5,1-6

--- 1-5/index.php
+++ 1-5/index.php
@@ -1,5 +1,8 @@
 <?php
-$array["1"] = 'A', 'B', 'C','D', 'E', 'F', 'G', 'H', 'I', 'J', 'K';
-$array["2"] = 'K', 'J', 'I','H', 'G', 'F', 'E', 'D', 'C', 'B', 'A';
+$array = ['1'=> 'ABCDEFGHIJK', '2' => 'KJIHGFEDCBA'];
+//$array = ['ABCDEFGHIJK'=> '1', 'KJIHGFEDCBA' => '2']
+//$array["1"] = 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K';
+//$array["2"] = 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A';
 
-print $array . "<br>";
+print $array['1'] . "<br>";
+print $array['2'];
